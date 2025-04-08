f8c0b515956377919481d9535f7f42748d9ab6a2
Carl <carltrillo7@gmail.com>
2025-04-08 21:39:16 +0800

    aquatech 2

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { ref } from 'vue'
+
+const firstname = ref('')
+const lastname = ref('')
+const username = ref('')
+const password = ref('')
+
+</script>
+
+<template>
+  <v-responsive>
+  <v-app>
+    <v-container fluid class="d-flex flex-column fill-height pa-0" style="background-color: #dfeaf3;">
+     
+      <v-row
+  class="justify-space-between align-center"
+  style="background-color: #dfeaf3; height: .30cm; width: 100%; margin: 0;"
+>
+  <!-- Optional: Add logo or nav content here -->
+</v-row>
+
+
+      <!-- Main Content -->
+      <v-row class="flex-grow-1">
+        <!-- Left Side: Image and Logo -->
+        <v-col cols="12" md="6" class="d-flex align-center justify-center pa-10" style="background: url('/background.jpg'); background-size: cover;">
+          <div class="text-center">
+            <v-img src="/bottle-phone.png" max-width="300" class="mx-auto mb-4" />
+            <h1 class="font-weight-bold" style="font-size: 3rem;">
+              <span style="color:#5dade2">Aqua</span><span style="color:#333">tech</span>
+            </h1>
+          </div>
+        </v-col>
+
+        <!-- Right Side: Login Form -->
+        <v-col cols="12" md="6" class="d-flex align-center justify-center" style="background-color: #dfeaf3;">
+          <v-card flat class="pa-6" width="350">
+            <v-text-field v-model="firstname" label="First Name" outlined />
+            <v-text-field v-model="lastname" label="Last Name" outlined />
+            <v-text-field v-model="username" label="Create Username" outlined />
+            <v-text-field v-model="password" label="Create Password" type="password" outlined />
+            <v-btn color="green" block class="mt-4">Register</v-btn>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <!-- Footer -->
+      <v-row
+  class="justify-center align-center"
+  style="background-color: #dfeaf3; height: .30cm; width: 100%; margin: 0;"
+>
+</v-row>
+    </v-container>
+  </v-app>
+</v-responsive>
+</template>
+
+<style scoped>
+.fill-height {
+  height: 100vh;
+}
+</style>
